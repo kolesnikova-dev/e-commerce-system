4da834ad8a530a30045408ee2cfbfc79c0c3f0ac
Nika Kolesnikova <kolesnikova.ve.0@gmail.com>
2026-03-22 16:46:33 -0400

    feat(utils): Add taxCalculator function

---
+++ src/utils/taxCalculator.ts
@@ -0,0 +1,13 @@
+// Return the dollar amount that a product is taxed at
+const GROCERIES = "groceries";
+const TAX_RATES = {
+    groceries: 3,
+    standard: 4.75,
+}
+
+const calculateTax = (price: number, category: string): number => {
+    if (category === GROCERIES) {
+        return price * (TAX_RATES.groceries / 100);
+    }
+    return price * (TAX_RATES.standard / 100);
+}
